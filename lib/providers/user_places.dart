523f628a05a28256dcd575a712083bc494c01bc9
unknown <mohamedamine.koubaa@esprit.tn>
2025-11-06 13:55:11 +0100

    storing data on mobile storage

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,15 +1,66 @@
 import 'dart:io';
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:favourite_places_app/models/place.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
+Future <Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, loc_lat REAL, loc_lng REAL, address TEXT)',
+      );
+    },
+    version: 1,
+  );
+  return db;
+}
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image,PlaceLocation location) {
-    final newPlace = Place(title: title, image: image, location: location);
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => Place(
+            id : row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+              latitude: row['loc_lat'] as double,
+              longitude: row['loc_lng'] as double,
+              address: row['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+    state = places;
+  }
+
+  void addPlace(String title, File image,PlaceLocation location)async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$fileName');
+    final newPlace = Place(title: title, image: copiedImage, location: location);
+    final db = await _getDatabase();
+      db.insert(
+        'user_places',
+        {
+          'id': newPlace.id,
+          'title': newPlace.title,
+          'image': newPlace.image.path,
+          'loc_lat': newPlace.location.latitude,
+          'loc_lng': newPlace.location.longitude,
+          'address': newPlace.location.address,
+        },
+      );
+
     state = [...state, newPlace];
   }
 }
--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -4,20 +4,34 @@ import 'package:favourite_places_app/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<PlacesScreen> createState() => _PlacesScreenState();
+}
 
-    final userPlaces = ref .watch(userPlacesProvider);
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userPlaces = ref.watch(userPlacesProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Your Places',
-          style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.onSurface),
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge
+              ?.copyWith(color: Theme.of(context).colorScheme.onSurface),
         ),
         centerTitle: false,
         elevation: 0,
@@ -39,8 +53,26 @@ class PlacesScreen extends ConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: PlacesList(
-          places: userPlaces,
+        child: FutureBuilder(
+          future: _placesFuture,
+          builder: (context, snapshot) => snapshot.connectionState ==
+                  ConnectionState.waiting
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : userPlaces.isEmpty
+                  ? Center(
+                      child: Text(
+                        'No places added yet. Start adding some!',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSurface
+                                  .withAlpha(200),
+                            ),
+                      ),
+                    )
+                  : PlacesList(places: userPlaces),
         ),
       ),
     );
